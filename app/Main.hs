3159edc2e532702234557bfe0f6e290cd4de8fbb
Matt Hunzinger <matthunz2@gmail.com>
2022-07-30 23:33:24 -0400

    Parse day1 data

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
+import Control.Monad.IO.Class
+import Text.Read
+import Data.Maybe
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    ints <- readInput
+    print ints
+
+readInput :: IO [Int]
+readInput = do
+    text <- readFile "data/day01.txt"
+    return $ mapMaybe readMaybe (lines text)
+
