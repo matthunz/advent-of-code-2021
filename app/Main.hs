8041474b8c81e721ae827d07b6cb29f4ba116cfc
Matt Hunzinger <matthunz2@gmail.com>
2022-07-31 12:56:34 -0400

    Solve day 2 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,38 @@ import Control.Monad.IO.Class
 import Data.Maybe (mapMaybe)
 import Text.Read
 
-data Instruction = Instruction String Int deriving (Show)
+data Instruction = Forward Int | Up Int | Down Int deriving (Show)
+
+instructions text = map g (f $ words text)
+  where
+    f :: [String] -> [([Char], Int)]
+    f (op : distance : xs) = (op, read distance) : f xs
+    f _ = []
+    g :: ([Char], Int) -> Instruction
+    g ("forward", d) = Forward d
+    g ("up", d) = Up d
+    g ("down", d) = Down d
 
 main :: IO ()
 main = do
   text <- readFile "data/day02.txt"
-  print $ h $ foldl g (0, 0) (f $ words text)
+  print $ h $ foldl g (0, 0, 0) (instructions text)
   where
-    f :: [String] -> [Instruction]
-    f (op : distance : xs) = Instruction op (read distance) : f xs
-    f _ = []
-    g (x, y) (Instruction "forward" d) = (x + d, y)
-    g (x, y) (Instruction "up" d) = (x, y - d)
-    g (x, y) (Instruction "down" d) = (x, y + d)
-    g (x, y) _ = (x, y)
+    g (x, y, a) (Forward d) = (x + d, a * d + y, a)
+    g (x, y, a) (Up d) = (x, y, a - d)
+    g (x, y, a) (Down d) = (x, y, a + d)
+
+    h (x, y, _) = x * y
+
+main3 :: IO ()
+main3 = do
+  text <- readFile "data/day02.txt"
+  print $ h $ foldl g (0, 0) (instructions text)
+  where
+    g (x, y) (Forward d) = (x + d, y)
+    g (x, y) (Up d) = (x, y - d)
+    g (x, y) (Down d) = (x, y + d)
+
     h (x, y) = x * y
 
 main2 = do
